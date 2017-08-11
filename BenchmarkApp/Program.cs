5be21ec7ee8bd934cc2354c88572358c98fb7c58
Martin Costello <martin@martincostello.com>
2017-08-11 12:42:20 +0100

    Benchmark with BenchmarkDotNet (#1)
    Benchmark with BenchmarkDotNet.
    Involves a few horrible hacks to get Razor views working properly.

--- BenchmarkApp/Program.cs
+++ BenchmarkApp/Program.cs
@@ -1,37 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
+using BenchmarkDotNet.Running;
 
 namespace BenchmarkApp
 {
-    internal class Program
+    internal static class Program
     {
-        private static readonly IDictionary<string, double> Results = new Dictionary<string, double>();
-        private const int TestRunCount = 1000;
-
-        private static void Main()
+        public static void Main(string[] args)
         {
-            RunTest(title: "No Template Service", url: "/no-template", testRuns: TestRunCount);
-            RunTest(title: "With Template Service", url: "/with-template", testRuns: TestRunCount);
-            RunTest(title: "With Cached Template Service", url: "/with-cached-template", testRuns: TestRunCount);
+            BenchmarkRunner.Run<ViewComponentBenchmarks>();
 
-            DisplayResults();
-        }
-
-        private static void RunTest(string title, string url, int testRuns)
-        {
-            using (var test = new ViewComponentBenchmarks(url, testRuns))
-            {
-                var time = test.RunTests().Result;
-                Results.Add(title, time);
-            }
-        }
-
-        private static void DisplayResults()
-        {
-            foreach (var result in Results)
-            {
-                Console.WriteLine($"{result.Key} ran {TestRunCount} times with an average time of {result.Value:N2} milliseconds.");
-            }
+            Console.Write("Press enter to exit...");
+            Console.ReadLine();
         }
     }
 }
